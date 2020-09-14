9c6cd4e7da1bcf17a5c7f89905886162a41f7906
Shashank Agarwal <shashanka141@gmail.com>
2020-09-14 21:59:13 +0530

    Creating Navbar

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NavbarComponent from './Components/Navbar';
+import {BrowserRouter,Route} from 'react-router-dom'
+import Home from './Components/screens/Home';
+import Login from './Components/screens/Login';
+import Signup from './Components/screens/Signup';
+import Profile from './Components/screens/Profile';
+
+function App() {
+  return (
+    <BrowserRouter>
+         <NavbarComponent/>
+         <Route exact path="/"><Home/></Route>
+         <Route path="/signin"><Login/></Route>
+         <Route path="/signup"><Signup/></Route>
+         <Route path="/profile"><Profile/></Route>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+
+const NavbarComponent = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Instagram</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+                    <NavLink className="nav-link" to="/signin">
+                        Login
+                    </NavLink>
+                </NavItem>
+            <NavItem>
+              <NavLink to="/signup" className="nav-link">SignUp</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/profile" className="nav-link">Profile</NavLink>
+            </NavItem>
+            {/* <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Profile
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown> */}
+          </Nav>
+          {/* <NavbarText>Simple Text</NavbarText> */}
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavbarComponent;
